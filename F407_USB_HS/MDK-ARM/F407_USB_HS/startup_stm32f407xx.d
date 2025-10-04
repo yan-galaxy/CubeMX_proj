36e0b77ecff98d9e7d739906b103bb1e8a88756f
galaxy <yanj3785@gmail.com>
2025-10-04 23:07:51 +0800

    USB-HS 杜邦线测试 电路干扰大，仅成功一次，程序没问题

---
+++ F407_USB_HS/MDK-ARM/F407_USB_HS/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+f407_usb_hs\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ F407_USB_HS/MDK-ARM/RTE/_F407_USB_HS/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'F407_USB_HS' 
+ * Target:  'F407_USB_HS' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
